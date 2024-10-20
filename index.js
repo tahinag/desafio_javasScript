6b1d7a4542d9be952b04327bcb1494a2028a62e7
Tahina Rodriguez <tahinagabrielarodriguezcobo@gmail.com>
2024-10-20 20:23:10 -0300

    se agrega desafío Java Script

---
+++ index.js
@@ -0,0 +1,8 @@
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+cant = document.querySelector('.cantidad')
+cont=Number(cant.innerHTML)
+precioFinal=document.querySelector('.valor-total')
